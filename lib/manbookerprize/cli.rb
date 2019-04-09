b58b96464e481d9b870e91808016fa09847db14f
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-09 21:46:50 +0000

    variable changes

--- lib/manbookerprize/cli.rb
+++ lib/manbookerprize/cli.rb
@@ -1,8 +1,40 @@
 class CommandLineInterface
   
   def call
+    greeting
+    menu
+    goodbye
+  end
+    
+  def greeting  
     puts "********** Welcome to the Man Booker Prize 2018! *********"
   end
   
+  def menu
+    puts "What would you like to do? Type winner to see the winning book, type nominees to see the nominated books or type exit to leave."
+    
+    while input != "exit" 
+      input = gets.strip.downcase
+      case input 
+        when "winner"
+          list_winner
+        when "nominees"
+          list_nominees
+        else
+          puts "I don't understand, please try again."
+      end
+    end
+  end
   
+  def list_winner
+    puts "This is the winner"
+  end
+  
+  def list_nominees
+    puts "Here are the nominees"
+  end
+  
+  def goodbye
+    puts "Thank you for visiting! See you again soon."
+  end
 end
