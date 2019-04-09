240f0a14ed9ac73d8b9d07bed1ec0ffdf8d34d3e
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-09 20:55:05 +0000

    update cli

--- lib/manbookerprize.rb
+++ lib/manbookerprize.rb
@@ -1,6 +1,8 @@
-require "manbookerprize/version"
+require_relative "./manbookerprize/version"
 
 module Manbookerprize
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require './manbooker/cli'
---
+++ lib/manbookerprize/cli.rb
@@ -0,0 +1,8 @@
+class CommandLineInterface
+  
+  def call
+    puts "********** Welcome to the Man Booker Prize 2018! *********"
+  end
+  
+  
+end
