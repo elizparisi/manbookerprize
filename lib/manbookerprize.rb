27177c400766716a360f0ccd8353d64254fd0cfc
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-09 00:40:29 +0000

    first commit

---
+++ lib/manbookerprize.rb
@@ -0,0 +1,6 @@
+require "manbookerprize/version"
+
+module Manbookerprize
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/manbookerprize/version.rb
@@ -0,0 +1,3 @@
+module Manbookerprize
+  VERSION = "0.1.0"
+end
