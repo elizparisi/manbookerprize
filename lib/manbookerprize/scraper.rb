0284de1c276a96c9f1331ab763c0acf8598d8bca
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-12 05:17:26 +0000

    updated scraper

--- lib/manbookerprize/scraper.rb
+++ lib/manbookerprize/scraper.rb
@@ -12,14 +12,17 @@ class Scraper
     winner = self.new
     winner.title = doc.search("h3.title a").text
     winner.author = doc.search("div.field-item.even p strong").text.gsub("Chair of judges Kwame Anthony Appiah comments:","").strip
-    winner.details = doc.search("div.field-name-body.field-type-text-with-summary.field-label-hidden").text
+    winner.details = doc.search("div.field-name-body.field-type-text-with-summary.field-label-hidden").text.gsub("\n","").gsub("\t"," ").gsub("\u2018","").gsub("\u2019","").gsub("\u00A0"," ").gsub("\u2014","").gsub("\u2026","").strip
     winner
   end 
   
   def self.scrape_shortlist
     doc = Nokogiri::HTML(open("https://themanbookerprize.com/fiction/backlist/2018"))
-    page.css("div.carousel-container h4.title").each do |title|
-      Book.new(title.text)
+    nominees = []
+    nominee = self.new
+    nominee.title = doc.search("div.carousel-container h4.title").text
+    nominee.author = doc.search
+    nominee.details = 
     end
   end
 end
